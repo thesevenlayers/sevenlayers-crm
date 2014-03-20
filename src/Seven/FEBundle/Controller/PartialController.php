aea4a7f776f91d7c0f4f0700aeeb52f7935e1d28
thesevenlayers <rafael@thesevenlayers.net>
2014-03-20 18:37:41 +0200

    set default date value

--- src/Seven/FEBundle/Controller/PartialController.php
+++ src/Seven/FEBundle/Controller/PartialController.php
@@ -8,11 +8,6 @@ class PartialController extends Controller
 {
     public function getPartialServiceProviderListAction($sp = null)
     {
-        if($sp == null)
-        {
-            $sp = $this->getDoctrine()->getRepository("SevenFEBundle:ServiceProvider")->findAll();
-        }
-
         return $this->render("SevenFEBundle:Default/Partial:service_providers_list.html.twig", array(
                 "service_providers" => $sp
             ));
